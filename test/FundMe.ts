f632a80b5eebe994cd8a8b60e9d323a02eab53a1
cnerylozada <cnerylozada@cnerylozadas-MacBook-Pro.local>
2024-05-05 08:33:24 -0500

    feat: fund me was tested

--- test/FundMe.ts
+++ test/FundMe.ts
@@ -16,29 +16,62 @@ describe("testing FundMe Contract", function () {
     expect(dataFeed).to.eq(mockDataFeed);
   });
 
-  it("should fail if there was not enough funds sent", async () => {
-    const { fundMe } = await loadFixture(deployContract);
-    await expect(fundMe.fund()).to.revertedWith("Dint send you enough");
+  describe("fund function", () => {
+    it("should fail if there was not enough funds sent", async () => {
+      const { fundMe } = await loadFixture(deployContract);
+      await expect(fundMe.fund()).to.revertedWith("Dint send you enough");
+    });
+
+    it("should store funds from funders", async () => {
+      const { fundMe, otherAccount } = await loadFixture(deployContract);
+      const amountSent = ethers.parseEther("1");
+      await fundMe.connect(otherAccount).fund({ value: amountSent });
+      const amountReceived = await fundMe.addressToAmountFunded(
+        otherAccount.address
+      );
+      expect(amountSent.toString()).to.be.eq(amountReceived.toString());
+
+      const contractBalance = await ethers.provider.getBalance(fundMe);
+      expect(contractBalance).to.be.eq(amountSent);
+    });
+
+    it("should store funder address after fund", async () => {
+      const { fundMe, otherAccount } = await loadFixture(deployContract);
+      const amountSent = ethers.parseEther("1");
+      await fundMe.connect(otherAccount).fund({ value: amountSent });
+
+      const firstFunderIndex = 0;
+      const funderAddress = await fundMe.funders(firstFunderIndex);
+      expect(funderAddress).to.be.eq(otherAccount.address);
+    });
   });
 
-  it("should store funds from funders", async () => {
-    const { fundMe, otherAccount } = await loadFixture(deployContract);
-    const amountSent = ethers.parseEther("1");
-    await fundMe.connect(otherAccount).fund({ value: amountSent });
+  describe("withdraw function", () => {
+    it("should not withdraw when the owner dint perform it", async () => {
+      const { fundMe, otherAccount } = await loadFixture(deployContract);
+      await expect(fundMe.connect(otherAccount).withdraw()).to.be.rejectedWith(
+        "You are not the owner"
+      );
+    });
 
-    const amountReceived = await fundMe.addressToAmountFunded(
-      otherAccount.address
-    );
-    expect(amountSent.toString()).to.be.eq(amountReceived.toString());
-  });
+    it("should update contract balance after withdraw", async () => {
+      const { fundMe, deployer, otherAccount } = await loadFixture(
+        deployContract
+      );
+      const amountSent = ethers.parseEther("1");
+      await fundMe.connect(otherAccount).fund({ value: amountSent });
 
-  it("should store funder address after fund", async () => {
-    const { fundMe, otherAccount } = await loadFixture(deployContract);
-    const amountSent = ethers.parseEther("1");
-    await fundMe.connect(otherAccount).fund({ value: amountSent });
-
-    const firstFunderIndex = 0;
-    const funderAddress = await fundMe.funders(firstFunderIndex);
-    expect(funderAddress).to.be.eq(otherAccount.address);
+      const startingContractBalance = await ethers.provider.getBalance(fundMe);
+      const startingDeployerBalance = await ethers.provider.getBalance(
+        deployer
+      );
+      const withdrawTx = await fundMe.withdraw();
+      const { gasPrice, gasUsed } = await withdrawTx.wait(1);
+      const gasCost = (gasPrice as bigint) * (gasUsed as bigint);
+      const endingDeployerBalance = await ethers.provider.getBalance(deployer);
+      expect(startingContractBalance + startingDeployerBalance).to.be.eq(
+        endingDeployerBalance + gasCost
+      );
+    });
   });
 });
