1c8e1dae559d2f0927c2e45eb517ccaffaa86ba5
cnerylozada <cnerylozada@cnerylozadas-MacBook-Pro.local>
2024-06-24 09:14:36 -0500

    fix: remove wrong file

--- utils/contracts/multiRaffle.ts
+++ utils/contracts/multiRaffle.ts
@@ -5,7 +5,7 @@ export const randomRequestParameters = [
     blockChainId: blockChainId.localhost,
     subscriptionId: "",
     vrfCoordinatorAddress: "",
-    callbackGasLimit: 79000,
+    callbackGasLimit: 100000,
     keyHash:
       "0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae",
   },
@@ -14,7 +14,7 @@ export const randomRequestParameters = [
     subscriptionId:
       "98781142419196898069637439385605607317145241753854318841858742632836163104000",
     vrfCoordinatorAddress: "0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B",
-    callbackGasLimit: 40000,
+    callbackGasLimit: 100000,
     keyHash:
       "0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae",
   },
