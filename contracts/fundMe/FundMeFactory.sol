d223d3ea85dccbe43389d772869b08ab71661db2
cnerylozada <cnerylozada@gmail.com>
2025-04-20 18:50:29 -0500

    refact: fundme-factory

--- contracts/fundMe/FundMeFactory.sol
+++ contracts/fundMe/FundMeFactory.sol
@@ -3,15 +3,24 @@ pragma solidity ^0.8.28;
 import {FundMe} from "./FundMe.sol";
 
 contract FundMeFactory {
+    struct BaseMetadata {
+        string title;
+        string description;
+        uint minAmountInUsd;
+        address priceFeedAddress;
+        int priceFeedDecimals;
+    }
     event NewCrowdFunding(
-        address indexed _address,
-        uint _createdAt,
-        string _title,
-        string _description,
-        uint _minAmountInUsd,
-        address _priceFeedAddress,
-        int _priceFeedDecimals
+        address indexed contractAddress,
+        uint createdAt,
+        BaseMetadata metadata
     );
+    struct Metadata {
+        address contractAddress;
+        uint createdAt;
+        BaseMetadata metadata;
+    }
+    Metadata[] s_newCrowdFunding;
 
     function createNewCrowdFunding(
         string calldata _title,
@@ -28,14 +37,24 @@ contract FundMeFactory {
         );
         uint createdAt = block.timestamp;
         address contractAddress = address(fundMe);
-        emit NewCrowdFunding(
-            contractAddress,
-            createdAt,
-            _title,
-            _description,
-            _minAmountInUsd,
-            _priceFeedAddress,
-            _priceFeedDecimals
+        BaseMetadata memory baseMetadata = BaseMetadata({
+            title: _title,
+            description: _description,
+            minAmountInUsd: _minAmountInUsd,
+            priceFeedAddress: _priceFeedAddress,
+            priceFeedDecimals: _priceFeedDecimals
+        });
+        emit NewCrowdFunding(contractAddress, createdAt, baseMetadata);
+        s_newCrowdFunding.push(
+            Metadata({
+                contractAddress: contractAddress,
+                createdAt: createdAt,
+                metadata: baseMetadata
+            })
         );
+    }
+
+    function contractsCreated() external view returns (Metadata[] memory) {
+        return s_newCrowdFunding;
     }
 }
--- contracts/storage/StorageFactory.sol
+++ contracts/storage/StorageFactory.sol
@@ -3,27 +3,20 @@ pragma solidity ^0.8.28;
 import {Storage} from "./Storage.sol";
 
 contract StorageFactory {
-    address[] private s_contractAddresses;
-
-    event NewContractCreated(address _address, string _course);
+    event NewContractCreated(address contractAddress, string course);
     struct Medatada {
-        address _address;
-        string _course;
+        address contractAddress;
+        string course;
     }
     Medatada[] s_newContractCreated;
-
-    function getAddressList() external view returns (address[] memory) {
-        return s_contractAddresses;
-    }
 
     function createStorage(string calldata _course) external {
         Storage storageContract = new Storage(_course);
         address newAddress = address(storageContract);
         emit NewContractCreated(newAddress, _course);
-        s_contractAddresses.push(newAddress);
 
         s_newContractCreated.push(
-            Medatada({_course: _course, _address: newAddress})
+            Medatada({course: _course, contractAddress: newAddress})
         );
     }
 
